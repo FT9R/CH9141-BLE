eb5ba21e5083957979c58b3386ef0fba46be5621
FT9R <ft9r@mail.ru>
2025-03-20 18:51:55 +0300

    platform functions separated from demo

--- ch9141/demo/ch9141_demo.h
+++ ch9141/demo/ch9141_demo.h
@@ -1,11 +1,7 @@
 #pragma once
 
 #include "../driver/ch9141.h"
-#include "usart.h"
-
-#define CH9141_MIN_DELAY  2
-#define CH9141_RX_TIMEOUT 2000
-#define CH9141_TX_TIMEOUT 2000
+#include "../interface/stm32/ch9141_ifc.h"
 
 /**
  * @brief CH9141 driver basic functionality demonstration
